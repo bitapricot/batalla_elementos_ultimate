3543d57374c78f1b59ffa0b18d2898f996d4d60c
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-09 06:26:30 -0300

    Update arista.h

--- arista.h
+++ arista.h
@@ -1,6 +1,5 @@
 #ifndef ARISTA_H_INCLUDED
 #define ARISTA_H_INCLUDED
-
 #include "vertice.h"
 
 class Arista {
@@ -9,10 +8,10 @@ private:
     Vertice* destino;
     int costo;
 public:
-    Arista(Vertice origen, Vertice destino, int costo);
+    Arista(Vertice* origen, Vertice* destino, int costo);
+    Arista(Vertice* origen, Vertice* destino);
     Vertice* obtener_destino();
     Vertice* obtener_origen();
     int obtener_costo();
     void insertar_costo(int costo);
-    
 }; #endif // ARISTA_H_INCLUDED
