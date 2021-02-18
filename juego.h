cba13499d495fcf33f956699e9da59abf354e12a
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-17 23:21:12 -0300

    Update juego.h

--- juego.h
+++ juego.h
@@ -1,12 +1,21 @@
 #ifndef JUEGO_H_INCLUDED
 #define JUEGO_H_INCLUDED
 
+#include "grafo.h"
+#include "jugador.h"
+
+const int PRIMER_TURNO_J1 = 1;
+const int PRIMER_TURNO_J2 = 2;
+
 class Juego {
 private:
     Grafo* grafito;
     Jugador jugador_1;
     Jugador jugador_2;
+    int primer_turno;
 public:
+    Grafo();
+    ~Grafo();
     void atacar();
     void cargar_partida();
     void cargar_tablero();
@@ -15,5 +24,7 @@ public:
     void guardar_partida();
     void imprimir_tablero();
     void moverse();
+    void determinar_primer_turno();
+    void turnar();
 };
 #endif // JUEGO_H_INCLUDED
