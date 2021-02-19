ace0818238f316d63da94e13ea362e9b229f0df9
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-19 08:48:29 -0300

    Update personaje_de_tierra.h

--- personaje_de_tierra.h
+++ personaje_de_tierra.h
@@ -27,7 +27,7 @@ public:
     Personaje_de_tierra(string nombre);
     
     
-    Personaje_de_tierra(string nombre, int escudo, int vidas, int energia, int fila, int columna, int id_jugador);
+    Personaje_de_tierra(string nombre, int escudo, int vidas, int energia, int fila, int columna, int id_jugador, bool se_defiende);
 
     /*
     Incrementa la energia segun la energia recuperada determinada en personaje.h
