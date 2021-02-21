bf8736b87210021154bfe3bf533b15719b56aec5
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-21 18:29:03 -0300

    Update nodo_diccionario.cpp

--- nodo_diccionario.cpp
+++ nodo_diccionario.cpp
@@ -1,13 +1,5 @@
 #include "nodo_diccionario.h"
 
-/*Nodo_diccionario::Nodo_diccionario(Clave clave, Valor valor, Nodo_diccionario* padre) {
-    this->clave = clave;
-    this->valor = valor;
-    this->izq = NULL;
-    this->der = NULL;
-    this->padre = padre;
-}*/
-
 Nodo_diccionario::Nodo_diccionario(Clave clave, Valor valor) {
     this->clave = clave;
     this->valor = valor;
@@ -24,24 +16,9 @@ Valor Nodo_diccionario::obtener_valor() {
     return this->valor;
 }
 
-
-/*void Nodo_diccionario::asignar_der(Nodo_diccionario* der, Nodo_diccionario* padre) {
-    this->der = der;
-    this->padre = padre;
-}*/
-
 void Nodo_diccionario::asignar_der(Nodo_diccionario* der) {
     this->der = der;
 }
-
-/*void Nodo_diccionario::asignar_izq(Nodo_diccionario* izq, Nodo_diccionario* padre) {
-    this->izq = izq;
-    this->padre = padre;
-}*/
-
-/*void Nodo_diccionario::asignar_padre(Nodo_diccionario *padre) {
-    this->padre = padre;
-}*/
 
 void Nodo_diccionario::asignar_clave(Clave nueva) {
     this->clave = nueva;
@@ -50,7 +27,6 @@ void Nodo_diccionario::asignar_clave(Clave nueva) {
 void Nodo_diccionario::asignar_valor(Valor nuevo) {
     this->valor = nuevo;
 }
-
 
 void Nodo_diccionario::asignar_izq(Nodo_diccionario* izq) {
     this->izq = izq;
@@ -64,10 +40,6 @@ Nodo_diccionario* Nodo_diccionario::obtener_izq() {
     return this->izq;
 }
 
-/*Nodo_diccionario* Nodo_diccionario::obtener_padre() {
-    return this->padre;
-}*/
-
 bool Nodo_diccionario::es_hoja() {
     return (!this->obtener_izq() && !this->obtener_der());
 }
@@ -80,7 +52,6 @@ bool Nodo_diccionario::solo_hijo_izq() {
     return (this->obtener_izq() && !this->obtener_der());
 }
 
-
-/*Nodo_diccionario::~Nodo_diccionario() {
+Nodo_diccionario::~Nodo_diccionario() {
     if(valor) delete valor;
-}*/
+}
