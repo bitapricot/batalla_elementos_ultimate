b0f838a02098adceee60c78fba4c0beb5b678896
Frank <74013151+mrinkspot@users.noreply.github.com>
2021-02-23 16:03:03 -0300

    Update precipicio.h

--- precipicio.h
+++ precipicio.h
@@ -7,7 +7,7 @@ class Precipicio: public Casillero
 {
 public:
     Precipicio();
-    int calcular_costo(Personaje* nuevo);
+    int calcular_costo(string elemento_pj);
     int obtener_costo_base();
     string tipo_casillero();
 };
