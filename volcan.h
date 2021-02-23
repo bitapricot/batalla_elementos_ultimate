3915452cd32e394c70ccb880db2b38434ce598da
Frank <74013151+mrinkspot@users.noreply.github.com>
2021-02-23 16:04:19 -0300

    Update volcan.h

--- volcan.h
+++ volcan.h
@@ -7,7 +7,7 @@ class Volcan: public Casillero
 {
 public:
     Volcan();
-    int calcular_costo(Personaje* nuevo);
+    int calcular_costo(string elemento_pj);
     int obtener_costo_base();
     string tipo_casillero();
 };
