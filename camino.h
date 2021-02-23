351472276fafec7ea92cc3c53d537d6e5084ecc9
Frank <74013151+mrinkspot@users.noreply.github.com>
2021-02-23 15:59:14 -0300

    Update camino.h

--- camino.h
+++ camino.h
@@ -5,7 +5,7 @@
 class Camino: public Casillero {
 public:
     Camino();
-    int calcular_costo(Personaje* nuevo);
+    int calcular_costo(string elemento_pj);
     int obtener_costo_base();
     string tipo_casillero();
 };
