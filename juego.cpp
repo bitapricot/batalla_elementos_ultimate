c285912ad6d2f2582903f57ae8c3c39d871f8894
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-24 16:55:15 -0300

    juego termina2

--- juego.cpp
+++ juego.cpp
@@ -24,11 +24,11 @@ int Juego::turnar() {
 }
 
 bool Juego::validar_ingreso(int valor) {
-	if (!(valor >= 0 && valor < DIM_FILA)){
-		cout << "Valor fuera de rango, reintentar." << endl;
-		return false;
-	} 
-	return true;
+    if (!(valor >= 0 && valor < DIM_FILA)){
+        cout << "Valor fuera de rango, reintentar." << endl;
+        return false;
+    } 
+    return true;
 }
 
 Coordenada Juego::pedir_coord() {
@@ -54,45 +54,160 @@ Coordenada Juego::pedir_coord() {
 
 int Juego::energia_minima(string elemento, bool accion) {
     if (accion) { // atacar
-	if (elemento == ELEMENTO_AGUA) return MIN_ENERGIA_ATAQUE_AGUA;
-	else if (elemento == ELEMENTO_AIRE) return MIN_ENERGIA_ATAQUE_AIRE;
-	else if (elemento == ELEMENTO_FUEGO) return MIN_ENERGIA_ATAQUE_FUEGO;
-	else return MIN_ENERGIA_ATAQUE_TIERRA
-		
+    if (elemento == ELEMENTO_AGUA) return MIN_ENERGIA_ATAQUE_AGUA;
+    else if (elemento == ELEMENTO_AIRE) return MIN_ENERGIA_ATAQUE_AIRE;
+    else if (elemento == ELEMENTO_FUEGO) return MIN_ENERGIA_ATAQUE_FUEGO;
+    else return MIN_ENERGIA_ATAQUE_TIERRA
+        
     } else { // defender
-	if (elemento == ELEMENTO_AGUA) return MIN_ENERGIA_DEFENSA_AGUA;
-	else if (elemento == ELEMENTO_AIRE) return MIN_ENERGIA_DEFENSA_AIRE;
-	else if (elemento == ELEMENTO_FUEGO) return MIN_ENERGIA_DEFENSA_FUEGO;
-	else return MIN_ENERGIA_DEFENSA_TIERRA
+    if (elemento == ELEMENTO_AGUA) return MIN_ENERGIA_DEFENSA_AGUA;
+    else if (elemento == ELEMENTO_AIRE) return MIN_ENERGIA_DEFENSA_AIRE;
+    else if (elemento == ELEMENTO_FUEGO) return MIN_ENERGIA_DEFENSA_FUEGO;
+    else return MIN_ENERGIA_DEFENSA_TIERRA
     }
 }
-	
 
-
-/*void Juego::atacar(int pos_personaje) {
+void Juego::atacar(int pos_personaje) {
     chequear_subturno();
     int turno_act = turnar();
     Jugador* jugador_act = jugadores[turno_act];
     Personaje* personaje_act = jugador_act[pos_personaje];
+    Coordenada coord_act = personaje_act->obtener_coordenadas();
+    
     if(personaje_act->de_que_elemento_soy() == AGUA) {
         if(personaje_act->obtener_energia() >= MIN_ENERGIA_PJ_AGUA) {
-            Coordenada nueva = pedir_coord_ataque();
-            if(tablero->acceder_tablero(nueva->obtener_primera(), nueva->obtener_segunda())->obtener_casillero()->obtener_personaje()) {
-                tablero->acceder_tablero(nueva->obtener_primera(), nueva->obtener_segunda())->obtener_casillero()->obtener_personaje()->recibe_ataque(personaje_act->de_que_elemento_soy());
-            } else cout << ERROR_NO_HAY_PERSONAJE << endl;
+            Coordenada nueva = pedir_coord();
+            ataque_p_agua(nueva);
         } else cout << ERROR_ENERGIA_INSUFICIENTE << endl;
     } else if(personaje_act->de_que_elemento_soy() == TIERRA) {
-        Coordenada coord_act = personaje_act->obtener_coordenadas();
-        ataque_pj_tierra(coord_act); // ubicar las coordenadas de los pj enemigos y calcular el daño en base a la distancia
+        if(personaje_act->obtener_energia() >= MIN_ENERGIA_PJ_TIERRA) {
+            Coordenada nueva = pedir_coord();
+            ataque_p_tierra(coord_act, nueva);
+        } else cout << ERROR_ENERGIA_INSUFICIENTE << endl;
+    } else if(personaje_act->de_que_elemento_soy() == FUEGO) {
+        if(personaje_act->obtener_energia() >= MIN_ENERGIA_PJ_FUEGO) {
+            ataque_p_fuego(personaje_act);
+        } else cout << ERROR_ENERGIA_INSUFICIENTE << endl;
+    } else {
 
     }
-}*/
+}
 
-/*void Juego::chequear_subturno() {
+void Juego::ataque_p_tierra(Coordenada coord_act, Coordenada enemigo) {
+    int distancia;
+    int danio;
+    if(tablero->acceder_tablero(enemigo)->hay_personaje()) {
+        distancia = costo_camino_minimo(coord_act, enemigo);
+        if(distancia <= 2) danio = 30;  // aca deberiamos poner las constantes!!!
+        else if(2 < distancia <= 4) danio = 20;
+        else danio = 10;
+        tablero->acceder_tablero(enemigo)->obtener_personaje()->recibe_ataque(TIERRA, danio);
+    } else cout << "no hay personaje en esa posicion" << endl;
+}
+
+void Juego::ataque_p_fuego(Coordenada coord_act) {
+    if(turnar() == JUGADOR_1) {
+        for(int i = 0; i < jugadores[JUGADOR_2]->obtener_cantidad_personajes(), i++) {
+            Personaje* enemigo = jugadores[JUGADOR_2]->obtener_personaje(i);
+            if(coord_act.obtener_primera == enemigo->obtener_coordenadas().obtener_primera() || coord_act.obtener_primera() + 1 == enemigo->obtener_coordenadas().obtener_primera() || coord_act.obtener_primera() - 1 == enemigo->obtener_coordenadas().obtener_primera()) {
+                enemigo->obtener_coordenadas()->recibe_ataque(FUEGO, ATAQUE_BASE_FUEGO);
+            }
+        }
+    } else {
+        for(int i = 0; i < jugadores[JUGADOR_1]->obtener_cantidad_personajes(), i++) {
+            Personaje* enemigo = jugadores[JUGADOR_1]->obtener_personaje(i);
+            Personaje* enemigo->obtener_coordenadas() = jugadores[JUGADOR_1]->obtener_personaje(i);
+            if(coord_act.obtener_primera == enemigo->obtener_coordenadas().obtener_primera() || coord_act.obtener_primera() + 1 == enemigo->obtener_coordenadas().obtener_primera() || coord_act.obtener_primera() - 1 == enemigo->obtener_coordenadas().obtener_primera()) {
+                enemigo->recibe_ataque(FUEGO, ATAQUE_BASE_FUEGO);
+            }
+        }
+}
+
+void Juego::ataque_p_agua(Coordenada enemigo) {
+    if(tablero->acceder_tablero(enemigo)->obtener_casillero()->obtener_personaje()) { 
+        tablero->acceder_tablero(enemigo)->obtener_casillero()->obtener_personaje()->recibe_ataque(AGUA, ATAQUE_BASE_AGUA);
+    } else cout << ERROR_NO_HAY_PERSONAJE << endl;
+}
+
+void Juego::ataque_p_aire() {
+    if(turnar() == JUGADOR_1) {
+        for(int i = 0; i < jugadores[JUGADOR_2]->obtener_cantidad_personajes(), i++) {
+            jugadores[JUGADOR_2]->obtener_personaje(i)->recibe_ataque(AIRE, ATAQUE_BASE_AIRE);
+        }
+    } else {
+        for(int i = 0; i < jugadores[JUGADOR_1]->obtener_cantidad_personajes(), i++) {
+            jugadores[JUGADOR_1]->obtener_personaje(i)->recibe_ataque(AIRE, ATAQUE_BASE_AIRE);
+        }
+    }
+}
+
+void Juego::defenderse() {
+    chequear_subturno();
+    int turno_act = turnar();
+    Jugador* jugador_act = jugadores[turno_act];
+    Personaje* personaje_act = jugador_act[pos_personaje];
+
+    if(personaje_act->de_que_elemento_soy() == AGUA) defensa_p_agua(personaje_act);
+    else if((personaje_act->de_que_elemento_soy() == AIRE) defensa_p_aire(personaje_act);
+    else if((personaje_act->de_que_elemento_soy() == FUEGO) personaje_act->defender();
+    else defensa_p_tierra();
+
+}
+
+void Juego::defensa_p_agua(Personaje* pj) {
+    pj->defender();
+    int id_jugador = pj->obtener_id_jugador();
+    Jugador* jugador_act = jugadores[id_jugador];
+    for(int i = 0; i < jugador_act; i++) {
+        if(jugador_act->obtener_personaje(i) != pj) jugador_act->obtener_personaje(i)->curar(10); // cambiar por una cte
+    }
+}
+
+void Juego::defensa_p_aire(Personaje* pj) {
+    pj->defender();
+    Coordenada nueva = pedir_coord();
+    if(!tablero->acceder_tablero(nueva) {
+        tablero->acceder_tablero(pj->obtener_coordenadas())->posicionar_personaje(0); 
+        pj->asignar_coordenadas_pj(nueva);
+        tablero->acceder_tablero(nueva)->obtener_casillero->posicionar_personaje(pj);
+    }
+    else cout << "ya hay un personaje en ese casillero" << endl;
+}
+
+void Juego::defensa_p_tierra(Personaje* pj) {
+    pj->defender();
+    pj->incrementar_escudo(true);
+}
+
+void Juego::moverse() {
+    chequear_subturno();
+    int turno_act = turnar();
+    Jugador* jugador_act = jugadores[turno_act];
+    Personaje* personaje_act = jugador_act[pos_personaje];
+    Vertice* vertice_actual = tablero->acceder_tablero(personaje_act->obtener_coordenadas());
+
+    Coordenada nueva = pedir_coord();
+
+    int energia_minima = tablero->costo_camino_minimo(personaje_act, tablero->acceder_tablero(nueva));
+    if(personaje_act->obtener_energia() >= energia_minima) {
+        if(!tablero->acceder_tablero(nueva)->obtener_casillero()->obtener_personaje()) {
+            Vertice** camino = tablero->camino_minimo(personaje_act, nueva);
+            int i = 0;
+            while(camino[i]) {
+                vertice_actual->obtener_casillero()->posicionar_personaje(0);
+                camino[i]->obtener_casillero()->posicionar_personaje(personaje_act);
+                personaje_act->asignar_coordenadas_personaje(camino[i]->obtener_coordenadas());
+                i++;
+            }
+        } else cout << "ya hay un personaje en ese casillero " << endl;
+    } else cout << "el personaje no tiene suficiente energia para moverse xd " << endl; // acÃ¡ en realidad habria que indicar cuanta energia tiene y cuanta necesita etc, nico dijo eso at least :pepiga:
+}
+
+void Juego::chequear_subturno() {
     int turno_act = turnar();
     Jugador* jugador_act = jugadores[turno_act];
     if(pos_personaje >= jugador_act->cantidad_personajes()) pos_personaje = 0;
-}*/
+}
 
 Juego::~Juego() {
     if (tablero) delete tablero;
