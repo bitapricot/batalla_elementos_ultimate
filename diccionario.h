e729bad8c1ad3966918ec6acd6f2b77615d8e7a4
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 18:36:15 -0300

    agrego pre y pos/chequear si aniquilamos los metodos comentados

--- diccionario.h
+++ diccionario.h
@@ -7,39 +7,80 @@
 class Diccionario
 {
 private:
-    // attributes
+    // atributos
     Nodo_diccionario* raiz;
     Nodo_diccionario* actual;
 
-    // methods
+private:
+    // metodos
+    
+    /*
+     PRE: nodo, clave, valor son de tipos validos
+     POS: da de alta el nodo en el diccionario
+     */ 
     Nodo_diccionario* insertar(Nodo_diccionario* nodo, Clave clave, Valor valor);
+    
+    /*
+     PRE: nodo es de tipo valido
+     POS: imprime el diccionario inorden
+     */
     void imprimir_inorden(Nodo_diccionario * nodo);
+    
+    /*
+     PRE: nodo y clave son de tipo valido
+     POS: busca el nodo en el diccionario
+     */
     Nodo_diccionario* buscar(Nodo_diccionario* nodo, Clave clave);
-    Clave buscar_min(Nodo_diccionario* nodo);
+    
+    
+    //Clave buscar_min(Nodo_diccionario* nodo);
     //Clave sucesor(Nodo_diccionario* nodo);
     //Nodo_diccionario* eliminar(Nodo_diccionario* nodo, Clave clave);
+    
+    /*
+     PRE: nodo es de tipo valido
+     POS: elimina todos los elementos del diccionario
+     */
     void eliminar_todos(Nodo_diccionario* nodo);
+    
+    /*
+     PRE: -
+     POS: pone actual en raiz
+     */
     void reiniciar();
 
+    /*
+     PRE: r y v son de tipo valido
+     POS: elimina el nodo del diccionario
+     */
     Nodo_diccionario* _eliminar(Nodo_diccionario* r, Clave v);
 
 
 public:
-    //methods
+    // metoodos
 
-    // Creates an vacio tree
-    Diccionario();
+    /*
+     PRE: -
+     POS: crea un diccionrio (arbol) vacio
+     */
+    Diccionario(); // constructor
 
-    // Adds a new nodo to the actual Diccionario. If its the tree is vacio
-    // the nodo insertared will be the raiz
+    /*
+     PRE: clave y valor son de tipo valido
+     POS: agrega un nuevo nodo a diccionario. El nodo sera raiz si el arbol esta vacio.
+     */
     void insertar(Clave clave, Valor valor);
 
-    // Prints all the data stored in the Diccionario, sorted from the
-    // smallest value to the greatest value.
+    /*
+     PRE: -
+     POS: imprime los elementos del diccionario en inorden, de menor a mayor.
+     */
     void imprimir_inorden();
 
-    // Finds a given value in the Diccionario. If the key exists it returns
-    // TRUE, otherwise it returns FALSE.
+    /*
+     PRE: clave es de tipo valido
+     POS: devuelve true si encuentra la clave en el diccionario, false de lo contrario.
+     */
     bool buscar(Clave clave);
 
     // Finds the minimum value that exist in the Diccionario.
@@ -51,23 +92,58 @@ public:
     // eliminars a given data from the Diccionario
     //void eliminar(Clave clave);
 
+    /*
+     PRE: -
+     POS: devuelve la raiz
+     */
     Nodo_diccionario* obtener_raiz();
 
+    /*
+     PRE: -
+     POS: devuelve true si el diccionario esta vacio, false de lo contrario
+     */
     bool vacio();
 
-    // Deletes all the nodos in the Diccionario
+    /*
+     PRE: -
+     POS: elimina todos los nodos del diccionario
+     */
     void eliminar_todos();
 
+    /*
+     PRE: clave es de tipo valido
+     POS: devuelve el valor asociado a la clave pasada por parametro
+     */
     Valor consulta(Clave clave);
 
+    /*
+     PRE: borrar es de tipo valido
+     POS: elimina el nodo asociado a la clave pasada por parametro
+     */
     void eliminar(Clave borrar);
 
+    /*
+     PRE: nodo es de tipo valido
+     POS: busca el minimo en el diccionario.
+     */
     Nodo_diccionario* minimo(Nodo_diccionario* nodo);
 
+    /*
+     PRE: nodo es de tipo valido
+     POS: busca el sucesor en el diccionario.
+     */
     Nodo_diccionario* buscar_sucesor(Nodo_diccionario* nodo);
 
+    /*
+     PRE: clave es de tipo valido
+     POS: devuelve true si existe el nodo asociado a dicha clave en el diccionario, false de lo contrario.
+     */
     bool esta(Clave clave);
 
-    ~Diccionario();
+    /*
+     PRE: -
+     POS: libera memoria
+     */
+    ~Diccionario(); // destructor
 };
 #endif //ABB_Diccionario_H
