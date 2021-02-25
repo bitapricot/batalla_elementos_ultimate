4b8b602aa4eff8d10e62d670ee44ca030881bcc3
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 15:48:52 -0300

    agrego pre y pos

--- coordenada.h
+++ coordenada.h
@@ -5,25 +5,63 @@ class Coordenada {
 private:
     int primera, segunda;
 public:
-    /*
-    Constructor base
-    */
-    Coordenada(): primera(0), segunda(0) {}
+    
+    /* 
+     PRE: -
+     POS: crea un objeto Coordenada con coord primera y segunda igual a 0.
+     */
+    Coordenada(): primera(0), segunda(0) {} // constructor base
 
-    Coordenada(const int primera, const int segunda);
+    /*
+     PRE: primera y segunda son enteros.
+     POS: crea un objeto Coordenada con coord primera y segunda pasada por parametro.
+     */
+    Coordenada(const int primera, const int segunda); // constructor
 
     /*
     Constructor de copia
     Coordenada(const Coordenada& c);*/
 
+    /*
+     PRE: c es un objeto de tipo Coordenada.
+     POS: realiza la suma entre dos objetos de tipo Coordenada.
+     */
     Coordenada operator+(Coordenada c);
 
+    /*
+     PRE: c es un objeto de tipo Coordenada.
+     POS: asigna c a otro objeto de tipo Coordenada.
+     */
     Coordenada operator=(Coordenada c);
-
-    int obtener_primera();
-    int obtener_segunda();
-    void cambiar_primera(int nueva);
-    void cambiar_segunda(int nueva);
+    
+    /*
+     PRE: c es un objeto de tipo Coordenada.
+     POS: compara c con otro objeto de tipo Coordenada.
+     */
     bool operator==(Coordenada c);
+
+    /*
+     PRE: -
+     POS: devuelve el valor de la primera coordenada del objeto.
+     */
+    int obtener_primera();
+    
+    /*
+     PRE: -
+     POS: devuelve el valor de la segunda coordenada del objeto.
+     */
+    int obtener_segunda();
+    
+    /*
+     PRE: nueva es de tipo entero.
+     POS: asigna nueva a primera.
+     */
+    void cambiar_primera(int nueva);
+    
+    /*
+     PRE: nueva es de tipo entero.
+     POS: asigna nueva a primera.
+     */
+    void cambiar_segunda(int nueva);
 };
 #endif // COORDENADA_H_INCLUDED
