02efb14f8b4175fa2e84cd3248851aac435fa85b
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 15:14:49 -0300

    agrego pre y pos

--- precipicio.h
+++ precipicio.h
@@ -6,9 +6,28 @@
 class Precipicio: public Casillero
 {
 public:
-    Precipicio();
+    /*
+     PRE: -
+     POS: crea un casillero de tipo precipicio.
+    */
+    Precipicio(); // constructor
+    
+    /*
+     PRE: elemento_pj es un string valido - agua, aire, fuego, tierra.
+     POS: devuelve el costo base del casillero de tipo precipicio.
+    */
     int calcular_costo(string elemento_pj);
+    
+    /*
+     PRE: -
+     POS: devuelve el costo base del casillero de tipo precipicio.
+    */
     int obtener_costo_base();
+    
+    /*
+     PRE: -
+     POS: devuelve "precipicio", el tipo de casillero.
+    */
     string tipo_casillero();
 };
 
