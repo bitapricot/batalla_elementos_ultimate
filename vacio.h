fdaed9ecad0d4df6a8acbc5250e21b521199fee4
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 15:17:08 -0300

    agrego pre y pos

--- vacio.h
+++ vacio.h
@@ -6,9 +6,28 @@
 class Vacio: public Casillero
 {
 public:
-    Vacio();
+    /*
+     PRE: -
+     POS: crea un casillero de tipo vacio.
+    */
+    Vacio(); // constructor
+    
+    /*
+     PRE: elemento_pj es un string valido - agua, aire, fuego, tierra.
+     POS: devuelve el costo base del casillero de tipo vacio.
+    */
     int calcular_costo(string elemento_pj);
+    
+    /*
+     PRE: -
+     POS: devuelve el costo base del casillero de tipo vacio.
+    */
     int obtener_costo_base();
+    
+    /*
+     PRE: -
+     POS: devuelve "vacio", el tipo de casillero.
+    */
     string tipo_casillero();
 };
 
