58d4eace7a06367660c59cded9cdbb9e41142248
Santiago TIssoni <santi.tissoni@gmail.com>
2021-02-24 21:26:25 -0300

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -91,7 +91,7 @@ void Juego::atacar(int pos_personaje) {
     int turno_act = turnar();
     Jugador* jugador_act = jugadores[turno_act];
     //Personaje* personaje_act = jugador_act[pos_personaje]; 
-    // ME DA ERROR EL VSCODE LA LINEA ANTERIOR, LINEA SIGUEINTE ES LO CORRECTO? SANTIAGO
+    // ME DA ERROR EL VSCODE LA LINEA ANTERIOR, LINEA SIGUIENTE ES LO CORRECTO? SANTIAGO
     Personaje * personaje_act = jugador_act->obtener_personaje(pos_personaje);
     Coordenada coord_act = personaje_act->obtener_coordenadas();
     
