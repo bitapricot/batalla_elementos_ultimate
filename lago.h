da0af4d80348c29a091c61765d25ec05a1c0eaeb
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 15:09:17 -0300

    agrego pre y pos

--- lago.h
+++ lago.h
@@ -6,9 +6,28 @@
 class Lago: public Casillero
 {
 public:
+    /*
+     PRE: -
+     POS: crea un casillero de tipo lago
+    */
     Lago();
+    
+    /*
+     PRE: elemento_pj es un string valido - agua, aire, fuego, tierra.
+     POS: devuelve el costo base del casillero de tipo lago.
+    */
     int calcular_costo(string elemento_pj);
+    
+    /*
+     PRE: -
+     POS: devuelve el costo base del casillero de tipo lago.
+    */
     int obtener_costo_base();
+    
+    /*
+     PRE: -
+     POS: devuelve "lago", el tipo de casillero.
+    */
     string tipo_casillero();
 };
 
