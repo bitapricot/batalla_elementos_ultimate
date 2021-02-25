c8aaebd4d02fe55594ab717088ed7f3dfb6906d6
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 14:57:41 -0300

    agrego pre y pos

--- arista.h
+++ arista.h
@@ -6,17 +6,57 @@ class Vertice;
 
 class Arista {
 private:
+    // atributos
     Vertice* origen;
     Vertice* destino;
     int costo;
 public:
-    Arista();
-    Arista(Vertice* origen, Vertice* destino, int costo);
-    Arista(Vertice* origen, Vertice* destino);
+    /* 
+     * PRE: -
+     * POS: crea una arista con origen = 0, destino = 0 y costo = 0.
+     */
+    Arista(); // constructor
+    
+    /*
+     * PRE: -
+     * POS: crea una arista con origen, destino y costo pasados por parametro.
+     */
+    Arista(Vertice* origen, Vertice* destino, int costo); // constructor
+    
+    /*
+     * PRE: -
+     * POS: crea una arista con origen y destin pasados por parametro y costo = 0.
+     */
+    Arista(Vertice* origen, Vertice* destino); // constructor
+    
+    /*
+     * PRE: -
+     * POS: devuelve vertice destino.
+     */
     Vertice* obtener_destino();
+    
+    /*
+     * PRE: -
+     * POS: devuelve vertice origen.
+     */
     Vertice* obtener_origen();
+    
+    /*
+     * PRE: -
+     * POS: devuelve costo.
+     */
     int obtener_costo();
+    
+    /*
+     * PRE: costo es un entero valido.
+     * POS: asigna dicho costo a la arista
+     */
     void insertar_costo(int costo);
-    ~Arista();
+    
+    /*
+     * PRE: -
+     * POS: libera memoria.
+     */
+    ~Arista(); // destructor
 };
 #endif // ARISTA_H_INCLUDED
