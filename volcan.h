848aa6bf92f533eb1fc9926c60b58249442c4f92
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-25 15:18:37 -0300

    agrego pre y pos

--- volcan.h
+++ volcan.h
@@ -6,9 +6,29 @@
 class Volcan: public Casillero
 {
 public:
-    Volcan();
+    /*
+     PRE: -
+     POS: crea un casillero de tipo volcan.
+    */
+    Volcan(); // casillero
+    
+    /*
+     PRE: elemento_pj es un string valido - agua, aire, fuego, tierra.
+     POS: devuelve el costo base del casillero de tipo volcan.
+    */
     int calcular_costo(string elemento_pj);
+    
+    /*
+     PRE: -
+     POS: devuelve el costo base del casillero de tipo volcan.
+    */
     int obtener_costo_base();
+    
+     
+    /*
+     PRE: -
+     POS: devuelve "volcan", el tipo de casillero.
+    */
     string tipo_casillero();
 };
 
