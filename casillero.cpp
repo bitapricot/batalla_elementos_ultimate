390d4c318e619fb9361643851b311f2e59483eb8
Frank <74013151+mrinkspot@users.noreply.github.com>
2021-02-25 15:18:20 -0300

    Update casillero.cpp
    modifique posicionar_personaje haciendo que actualice las coordenadas del mismo

--- casillero.cpp
+++ casillero.cpp
@@ -8,8 +8,9 @@ Personaje* Casillero::obtener_personaje() {
     return personaje;
 }
 
-void Casillero::posicionar_personaje(Personaje* nuevo) {
+void Casillero::posicionar_personaje(Personaje* nuevo, Coordenada coordenada_actual) {
     personaje = nuevo;
+    nuevo->asignar_coordenadas(coordenada_actual);
 }
 
 Casillero::~Casillero() {
