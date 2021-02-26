adffcff87efbb87d02a0c016a6df48f4ab50d5e1
camilabartocci <65984154+camilabartocci@users.noreply.github.com>
2021-02-26 00:46:44 -0300

    agrego pre y pos

--- jugador.h
+++ jugador.h
@@ -15,14 +15,54 @@ private:
     int id;
     int cantidad_personajes;
 public:
-    Jugador();
+    /*
+     PRE: -
+     POS: crea un objeto jugador con cant_personajes = 0
+     */
+    Jugador(); // constructor
+    
+    /*
+     PRE: id debe ser de tipo entero
+     POS: crea un personaje con id pasado por parametro
+     */
     Jugador(int id);
-    ~Jugador();
+    
+    ~Jugador(); // destructor
+    
+    /*
+     PRE: nuevo debe ser de tipo Personaje*
+     POS: agrega un personaje al vector de personajes_elegidos
+     */
     void agregar_personaje(Personaje* nuevo);
+    
+    /*
+     PRE: -
+     POS: devuelve true si el jugador eligio el maximo de personajes, false de lo contrario
+     */
     bool eligio_max_personajes();
+    
+    /*
+     PRE: -
+     POS: devuelve true si todos los personajes del jugador tienen vida = 0, false de lo contrario
+     */
     bool jugador_perdio();
+    
+    /*
+     PRE: -
+     POS: devuelve cantidad de personajes vivos del jugador
+     */
     int obtener_cantidad_personajes();
+    
+    /*
+     PRE: -
+     POS: devuelve id del jugador
+     */
     int obtener_id();
+    
+    /*
+     PRE: pos es un entero valido
+     POS: devuelve el personaje ubicado en la posicion pos, en el vector de personajes del jugador
+     */
     Personaje* obtener_personaje(int pos);
 };
 
